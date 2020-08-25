00a884e33ab96219a54e1c024670ab743691e6be
Egor Dorichev <egordorichev@gmail.com>
2020-08-25 18:00:57 +0300

    Switched to std::string as input map keys (since it's the standard), otherwise emscripten is not happy with me

--- src/tsab/tsab_input.cpp
+++ src/tsab/tsab_input.cpp
@@ -452,14 +452,7 @@ static const char *controller_db =
 	"4d466947616d65706164020000000000,MFi Gamepad,a:b0,b:b1,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,leftshoulder:b4,rightshoulder:b5,start:b6,x:b2,y:b3,platform:iOS,\n"
 	"05000000de2800000511000001000000,Steam Controller,a:b0,b:b1,back:b6,guide:b8,leftshoulder:b4,leftstick:b9,lefttrigger:a2,leftx:a0,lefty:a1,rightshoulder:b5,righttrigger:a3,start:b7,x:b2,y:b3,platform:iOS,";
 
-// Compares two strings in a map
-struct compare {
-	bool operator() (char const *a, char const *b) {
-		return std::strcmp(a, b) < 0;
-	}
-};
-
-static std::map<const char *, int, compare> key_map;
+static std::map<std::string, int> key_map;
 
 static void setup_key_map() {
 	key_map["a"] = SDL_SCANCODE_A;
