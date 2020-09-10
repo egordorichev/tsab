92704de3c6e460ae102107f642e16bf4922ea5c5
Egor Dorichev <egordorichev@gmail.com>
2020-09-10 12:12:56 +0300

    build_html.sh now works from 0

--- build_html.sh
+++ build_html.sh
@@ -1,2 +1,18 @@
+git submodule update --init --recursive
+
+cd util
+cmake .
+make
+cd ..
+
+cd libs/lit/html/
+./build_html.sh
+cd ../../../
+
+cd libs/box2d/
+emcmake cmake -DBOX2D_BUILD_DOCS=OFF -DBOX2D_BUILD_TESTBED=OFF -DBOX2D_BUILD_UNIT_TESTS=OFF .
+emmake make
+cd ../../
+
 emcmake cmake .
 emmake make
