c06ee6d67b588e968dd7856b15cf8b39a87dec0d
Egor Dorichev <egordorichev@gmail.com>
2020-09-24 10:45:19 +0300

    Working on android support

--- build_deps.sh
+++ build_deps.sh
@@ -1,4 +1,5 @@
 git submodule update --init --recursive
+git submodule update --remote --merge
 
 cd util
 cmake .
@@ -7,5 +8,13 @@ cd ..
 
 cmake .
 cd libs/box2d/
-./build.sh
+
+# contents of ./build.sh + some flags
+rm -rf build
+mkdir build
+cd build
+cmake -DBOX2D_BUILD_DOCS=OFF -DBOX2D_BUILD_UNIT_TESTS=OFF -DBOX2D_BUILD_TESTBED=OFF ..
+cmake --build .
+# end contents
+
 cd ../../
